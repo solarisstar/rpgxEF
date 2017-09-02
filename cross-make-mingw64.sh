8b042971953ed67a464752d022025348bd093481
Maarten Thomassen <m.j.l.h.thomassen@gmail.com>
2017-09-02 12:27:58 +0200

    Fixed windows builds on travis
    # Conflicts:
    #	.travis.yml
    #	cross-make-mingw.sh
    #	cross-make-mingw64.sh

--- cross-make-mingw64.sh
+++ cross-make-mingw64.sh
@@ -2,32 +2,20 @@
 
 CMD_PREFIX="amd64-mingw32msvc x86_64-w64-mingw32";
 
-# Set CC to correct compiler
-if [ "X$CC" = "X" ]; then
-    for check in $CMD_PREFIX; do
-        full_check="${check}-gcc"
-	if [ ! $(which "$full_check") = "" ]; then
-	    export CC="$full_check"
-	fi
-    done
-fi
-
-# Set WINDRES to correct compiler
-if [ "X$WINDRES" = "X" ]; then
-    for check in $CMD_PREFIX; do
-        full_check="${check}-windres"
-	if [ ! $(which "$full_check") = "" ]; then
-	    export WINDRES="$full_check"
-	fi
-    done
-fi
-
-# Add mingw header files to PATH
 for check in $CMD_PREFIX; do
-    INCLUDE_DIR="/usr/${check}";
-    if [ ! $PATH = *"$INCLUDE_DIR"* ] && [ -d "$INCLUDE_DIR" ] ; then
-	    export PATH="$INCLUDE_DIR:$PATH"
-    fi
+	full_check="${check}-gcc"
+	if [ ! $(which "$full_check") = "" ]; then
+		export CC="$full_check"
+		MINGW_VERSION=${check};
+	fi
+done
+
+
+for check in $CMD_PREFIX; do
+	full_check="${check}-windres"
+	if [ ! $(which "$full_check") = "" ]; then
+		export WINDRES="$full_check"
+	fi
 done
 
 if [ "X$WINDRES" = "X" -o "X$CC" = "X" ]; then
